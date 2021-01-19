186882108a6da95c8dca5b81ce000fc3f8f3ca21
Leon Brin <brinl1@southernct.edu>
2021-01-19 08:49:15 -0500

    Exercise updates

--- octave/bisection.m
+++ octave/bisection.m
@@ -1,26 +1,26 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Written by Leon Brin                          10 December 2020 %
+% Written by Leon Brin                           11 January 2021 %
 % Purpose: This function implements the bisection method where   %
 %        the number of iterations is calculated, not supplied as %
 %        an argument                                             %
 % INPUT: interval [a,b]; function f; tolerance tol               %
-% OUTPUT: approximation p; f(p), FP; number of iterations N0     %
+% OUTPUT: approximation m; f(m), M; number of iterations i       %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-function [p,FP,N0] = bisection(a,b,f,tol)
-  FA = f(a);
-  N0 = ceil((log(abs(b-a))-log(tol))/log(2));
-  for i=1:N0
-    p = a + (b-a)/2;
-    FP = f(p);
-    if (FP == 0)
-      return
-    endif
-    if (FA*FP > 0)
-      a = p;
-      FA = FP;
-    else
-      b = p;
+function [m,M,i]=bisection(a,b,f,tol)
+  N=ceil((log(b-a)-log(tol))/log(2));
+  L=f(a);
+  for i=1:N
+    m=(a+b)/2;
+    M=f(m);
+    if (M==0)
+      return;
     end%if
-  end%while
+    if (L*M<0)
+      b=m;
+    else
+      a=m;
+      L=M;
+    end%if
+  end%for
+  i=N;
 end%function
